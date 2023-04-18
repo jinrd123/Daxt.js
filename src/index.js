b7c1a84cfdea19ced397fcd6811b550af76ba4d5
jinrongda <jinrongda@baidu.com>
2023-04-18 11:32:48 +0800

    feat: 构建基本的ssr服务端应用

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    `<html>
+        <head>
+            <title>hello</title>
+        </head>
+        <body>
+            <h1>hello Daxt</h1>
+            <p>start at 2023.4.18</p>
+        </body>
+    </html>`
+  );
+});
+
+app.listen(3000, () => {
+  console.log("server run successfully");
+});
