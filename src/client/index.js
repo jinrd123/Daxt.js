c77ffb8863f558e0e122af1a00560b1805d738e1
jinrongda <2368741842@qq.com>
2023-04-24 15:43:58 +0800

    feat: 配置redux-thunk

--- src/client/index.js
+++ src/client/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "../Routes";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 const reducer = (state = { name: "daxt" }, action) => {
   return state;
 };
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const App = () => {
   return (
--- src/server/utils.js
+++ src/server/utils.js
@@ -2,14 +2,15 @@ import React from "react"; // 提供jsx语法支持
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
 import Routes from "../Routes";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 export const render = (req) => {
   const reducer = (state = { name: "daxt" }, action) => {
     return state;
   };
-  const store = createStore(reducer);
+  const store = createStore(reducer, applyMiddleware(thunk));
 
   const content = renderToString(
     <Provider store={store}>
